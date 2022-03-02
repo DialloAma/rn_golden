231634a945f92273101d88d1f66cfbd14f2e5fe4
DIALLO <amadou9393@yahoo.fr>
2022-03-02 03:59:17 +0000

    making clear my action code

--- Redux/Actions.js
+++ Redux/Actions.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 export const AddClient=(newclt)=>{
-   // newclt.id=Math.random().toString();
     return async (dispatch)=>{
       await axios.post('http://192.168.43.119:2000/Clients', newclt)
       .then(res => {
@@ -18,37 +17,20 @@ export const AddClient=(newclt)=>{
     }
 }
 
-export const AllClients=()=>{
+export const getAllClients=()=>{
     return async (dispatch)=>{
-        
-            fetch('http://192.168.43.119:2000/Clients')
+           fetch('http://192.168.43.119:2000/Clients')
             .then(res=>res.json())
-            .then(result=>{
+            .then(data=>{
                 dispatch({
                     type:'ALL_CLT',
-                    payload: result
+                    payload: data
                  })
             }).catch(err=>console.log(err))
-          /*  const result= await axios.get('http://192.168.43.119:2000/Clients')
-            console.log(result.data)
-            dispatch({
-             type:'ALL_CLT',
-             payload: result.data
-            })*/
-            
-        
-      
-        
-
-    }
+             }
 }
 
 export const AddProdt=(newprod)=>{
-  /*  newprod.id=Math.random().toString()
-    return{
-        type:'ADD_PROD',
-        payload:newprod
-    }*/
     return async (dispatch)=>{
         await axios.post('http://192.168.43.119:2000/Product',newprod)
         .then((res)=>{
@@ -82,31 +64,9 @@ export const AddItems=(item,qtysold)=>{
            qtysold: qtysold
         }
     }
-   /* return async (dispatch,getState)=>{
-          // const {data} = await axios.get(`http://192.168.43.119:2000/Product/${itemid}`);
-          const {data} = await axios.get("http://192.168.43.119:2000/Product");
-           console.log(data)
-           const {cart : {Cartitems}} =getState();
-           dispatch({
-            type:'ADD_CART',
-            payload: {
-                id : data._id,
-                pname:data.pname,
-                price:data.price, 
-                qtysold: qtysold
-             }
-           })
-    }*/
+ 
 }
-export const UpdatQty=(qtysold,id)=>{
-    return{
-        type:'UPQTYSOLD',
-        payload: {
-            id,
-          qtysold:qtysold
-        }
-    }
-}
+
 
 export const AddDeliver=(newdel)=>{
     return{
